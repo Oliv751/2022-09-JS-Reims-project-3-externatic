dbe2be2aa8a80f731853b76e22aa498d6cb5fde6
FMichel5173 <fmichel81@sfr.fr>
2022-12-06 12:29:52 +0100

    created table offer in database.sql and modify router.js

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,13 @@
-CREATE TABLE item (
+CREATE TABLE offer (
   id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  offer_name varchar(100) NOT NULL,
+  location varchar(200) NOT NULL,
+  contract varchar(200) NOT NULL,
+  publication_date date
+)
+  engine=InnoDB DEFAULT charset=latin1;
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+
+INSERT INTO offer (offer_name, location, contract, publication_date) VALUES
+('lorem ipsum 1', 'lorem ipsum 1', 'lorem ipsum 1', '20221206'),
+('lorem ipsum 2', 'lorem ipsum 2', 'lorem ipsum 2', '20221206');
--- backend/src/router.js
+++ backend/src/router.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
 
-router.get("/items", itemControllers.browse);
-router.get("/items/:id", itemControllers.read);
-router.put("/items/:id", itemControllers.edit);
-router.post("/items", itemControllers.add);
-router.delete("/items/:id", itemControllers.destroy);
+router.get("/offers", itemControllers.browse);
+router.get("/offers/:id", itemControllers.read);
+router.put("/offers/:id", itemControllers.edit);
+router.post("/offers", itemControllers.add);
+router.delete("/offers/:id", itemControllers.destroy);
 
 module.exports = router;
