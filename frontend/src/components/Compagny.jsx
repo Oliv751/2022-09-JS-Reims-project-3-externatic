a6387cd1fa6ae8f5880a0d90d92d20e9f96b4a3e
valentin <marlois.valentin@gmail.com>
2022-12-06 14:20:06 +0100

    fix indentation

--- frontend/src/components/Compagny.jsx
+++ frontend/src/components/Compagny.jsx
@@ -3,18 +3,30 @@ export default function Compagny() {
     <form>
       <p>Join Externatic as entreprise</p>
       <div>
-        <label htmlFor="name">Nom</label>
-        <input id="name" type="text" /> <br />
-        <label htmlFor="phone">Telephone</label>
-        <input id="phone" type="number" /> <br />
-        <label htmlFor="mail">Email</label>
-        <input id="mail" type="email" /> <br />
-        <label htmlFor="siret">N sriret</label>
-        <input id="siret" type="number" /> <br />
-        <label htmlFor="password">Mot de passe</label>
-        <input id="password" type="password" /> <br />
-        <label htmlFor="passwordConfirm">Comfirmer mot de passe</label>
-        <input id="passwordConfirm" type="password" /> <br />
+        <div>
+          <label htmlFor="name">Nom</label>
+          <input id="name" type="text" />{" "}
+        </div>
+        <div>
+          <label htmlFor="phone">Telephone</label>
+          <input id="phone" type="number" />{" "}
+        </div>
+        <div>
+          <label htmlFor="mail">Email</label>
+          <input id="mail" type="email" />{" "}
+        </div>
+        <div>
+          <label htmlFor="siret">N sriret</label>
+          <input id="siret" type="number" />{" "}
+        </div>
+        <div>
+          <label htmlFor="password">Mot de passe</label>
+          <input id="password" type="password" />{" "}
+        </div>
+        <div>
+          <label htmlFor="passwordConfirm">Comfirmer mot de passe</label>
+          <input id="passwordConfirm" type="password" />{" "}
+        </div>
       </div>
 
       <button type="button">Creer un compte</button>
