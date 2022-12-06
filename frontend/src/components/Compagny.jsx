6893e9119aeee2952080982d4b8529ae4412db54
valentin <marlois.valentin@gmail.com>
2022-12-06 14:20:39 +0100

    fix indentation

--- frontend/src/components/Compagny.jsx
+++ frontend/src/components/Compagny.jsx
@@ -5,27 +5,27 @@ export default function Compagny() {
       <div>
         <div>
           <label htmlFor="name">Nom</label>
-          <input id="name" type="text" />{" "}
+          <input id="name" type="text" />
         </div>
         <div>
           <label htmlFor="phone">Telephone</label>
-          <input id="phone" type="number" />{" "}
+          <input id="phone" type="number" />
         </div>
         <div>
           <label htmlFor="mail">Email</label>
-          <input id="mail" type="email" />{" "}
+          <input id="mail" type="email" />
         </div>
         <div>
           <label htmlFor="siret">N sriret</label>
-          <input id="siret" type="number" />{" "}
+          <input id="siret" type="number" />
         </div>
         <div>
           <label htmlFor="password">Mot de passe</label>
-          <input id="password" type="password" />{" "}
+          <input id="password" type="password" />
         </div>
         <div>
           <label htmlFor="passwordConfirm">Comfirmer mot de passe</label>
-          <input id="passwordConfirm" type="password" />{" "}
+          <input id="passwordConfirm" type="password" />
         </div>
       </div>
 
