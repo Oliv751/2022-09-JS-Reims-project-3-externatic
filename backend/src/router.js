02efbc5ea5fe6a0d9239dd1d8f1603ba6501373f
FMichel5173 <fmichel81@sfr.fr>
2022-12-06 14:10:05 +0100

    Fix Pull Request errors

--- backend/src/router.js
+++ backend/src/router.js
@@ -10,10 +10,10 @@ router.put("/offers/:id", itemControllers.edit);
 router.post("/offers", itemControllers.add);
 router.delete("/offers/:id", itemControllers.destroy);
 
-router.get("/enterprises", itemControllers.browse);
-router.get("/enterprises/:id", itemControllers.read);
-router.put("/enterprises/:id", itemControllers.edit);
-router.post("/enterprises", itemControllers.add);
-router.delete("/enterprises/:id", itemControllers.destroy);
+router.get("/companies", itemControllers.browse);
+router.get("/companies/:id", itemControllers.read);
+router.put("/companies/:id", itemControllers.edit);
+router.post("/companies", itemControllers.add);
+router.delete("/companies/:id", itemControllers.destroy);
 
 module.exports = router;
