8b1f8f6c74140f67c9f34eac3c34d1cbf72c5c22
valentin <marlois.valentin@gmail.com>
2022-12-06 12:43:30 +0100

    add page for created compte and style this page

---
+++ frontend/src/components/Compagny.jsx
@@ -0,0 +1,23 @@
+export default function Compagny() {
+  return (
+    <section>
+      <p>Join Externatic as entreprise</p>
+      <div>
+        <label htmlFor="name">Nom</label>
+        <input id="name" type="text" /> <br />
+        <label htmlFor="phone">Telephone</label>
+        <input id="phone" type="number" /> <br />
+        <label htmlFor="mail">Email</label>
+        <input id="mail" type="email" /> <br />
+        <label htmlFor="siret">N sriret</label>
+        <input id="siret" type="number" /> <br />
+        <label htmlFor="password">Mot de passe</label>
+        <input id="password" type="password" /> <br />
+        <label htmlFor="passwordConfirm">Comfirmer mot de passe</label>
+        <input id="passwordConfirm" type="password" /> <br />
+      </div>
+
+      <button type="button">Creer un compte</button>
+    </section>
+  );
+}
---
+++ frontend/src/pages/CreateCompte.jsx
@@ -0,0 +1,6 @@
+import Compagny from "../components/Compagny";
+import "../styles/createCompte.scss";
+
+export default function CreateCompte() {
+  return <Compagny />;
+}
