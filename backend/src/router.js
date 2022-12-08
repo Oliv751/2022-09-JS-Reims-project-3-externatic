51f3c40dc70af6b7296a810cfcc892d866834c9a
FMichel5173 <fmichel81@sfr.fr>
2022-12-08 13:37:10 +0100

    Added read in offerControllers.js and router.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const offerControllers = require("./controllers/offerControllers");
 
 router.get("/offers", offerControllers.browse);
+router.get("/offers/:id", offerControllers.read);
 
 module.exports = router;
