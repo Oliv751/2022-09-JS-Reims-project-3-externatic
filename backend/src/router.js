c2de0aaef9484c0737450d5cb07d84cdc583672c
FMichel5173 <fmichel81@sfr.fr>
2022-12-08 15:46:50 +0100

    created companyControllers.js and added browse in companyControllers.js and router.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,11 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 const offerControllers = require("./controllers/offerControllers");
+const companyControllers = require("./controllers/companyControllers");
 
 router.get("/offers", offerControllers.browse);
 router.get("/offers/:id", offerControllers.read);
 router.put("/offers/:id", offerControllers.edit);
 router.post("/offers", offerControllers.add);
 router.delete("/offers/:id", offerControllers.destroy);
+
+router.get("/companies", companyControllers.browse);
 
 module.exports = router;
