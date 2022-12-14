9011f9bf627baf619c7ed20b227871cf832fca87
iturnmyscreenon <benoit.galey@gmail.com>
2022-12-14 17:09:10 +0100

    made modifications on the FormS components

---
+++ frontend/src/components/NewConsultantForm.jsx
@@ -0,0 +1,34 @@
+export default function NewConsultantForm() {
+  return (
+    <form>
+      <p>Join Externatic as entreprise</p>
+
+      <div>
+        <label htmlFor="name">Nom</label>
+        <input id="name" type="text" />
+      </div>
+      <div>
+        <label htmlFor="phone">Telephone</label>
+        <input id="phone" type="number" />
+      </div>
+      <div>
+        <label htmlFor="mail">Email</label>
+        <input id="mail" type="email" />
+      </div>
+      <div>
+        <label htmlFor="siret">N sriret</label>
+        <input id="siret" type="number" />
+      </div>
+      <div>
+        <label htmlFor="password">Mot de passe</label>
+        <input id="password" type="password" />
+      </div>
+      <div>
+        <label htmlFor="passwordConfirm">Comfirmer mot de passe</label>
+        <input id="passwordConfirm" type="password" />
+      </div>
+
+      <button type="submit">Creer un compte</button>
+    </form>
+  );
+}
--- frontend/src/pages/CreateAccount.jsx
+++ frontend/src/pages/CreateAccount.jsx
@@ -1,14 +1,15 @@
 import { useParams } from "react-router-dom";
-import Compagny from "../components/Compagny";
-import Candidate from "../components/Candidate";
+import NewConsultantForm from "../components/NewConsultantForm";
+import NewCandidateForm from "../components/NewCandidateForm";
+
 import "../styles/createAccount.scss";
 
 export default function CreateAccount() {
   const { type } = useParams();
   return (
     <div className="createAccount">
-      {type === "candidate" && <Candidate />}
-      {type === "company" && <Compagny />}
+      {type === "candidate" && <NewCandidateForm />}
+      {type === "company" && <NewConsultantForm />}
     </div>
   );
 }
