b6937ef41656aab2e03730d8af79604a2251e55f
AlexisCode22 <durinalexis@gmail.com>
2022-12-20 09:32:41 +0100

    resolve PR

--- backend/src/models/consultantManager.js
+++ backend/src/models/consultantManager.js
@@ -4,6 +4,13 @@ class ConsultantManager extends AbstractManager {
   constructor() {
     super({ table: "consultant" });
   }
+
+  insert(consultant) {
+    return this.connection.query(
+      `insert into ${this.table} ( firstname , lastname , user_id) value (? , ? , ?)`,
+      [consultant.firstname, consultant.lastname, consultant.id]
+    );
+  }
 }
 
 module.exports = ConsultantManager;
