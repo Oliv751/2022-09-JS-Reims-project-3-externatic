94be4441909ff314629ddf7dbc600cbc839ba96a
wild-valentin <marlois.valentin@gmail.com>
2023-01-04 12:23:42 +0100

    get all offers from the database and one offer by id for the page offer details

--- frontend/src/components/OfferCard.jsx
+++ frontend/src/components/OfferCard.jsx
@@ -6,16 +6,16 @@ import SampleCard from "./sampleCard";
 import "../styles/offercard-style.scss";
 
 export default function OfferCard({ offer }) {
+  const date = new Date(offer.publication_date);
+
   return (
     <li>
       <Link to={`/offers/${offer.id}`} className="cardsOffer">
         <section>
           <div className="description">
-            <div>
-              <h2>{offer.offerName}</h2>
-              <h3>{offer.companyName}</h3>
-            </div>
-            <p>offre publie le {offer.publicationDate}</p>
+            <h2>{offer.offer_name}</h2>
+
+            <p>offre publie le {date.toLocaleDateString("fr-FR")}</p>
           </div>
           <div>
             <MdLocationOn className="icon_description" />
