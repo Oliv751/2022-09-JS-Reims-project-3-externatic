0870a5dc46123e9b4f17e983f0f9bb7c54ca3160
iturnmyscreenon <benoit.galey@gmail.com>
2023-01-04 15:15:31 +0100

    made login auth in frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthContextProvider } from "./pages/AuthContext";
 import OfferDetails from "./pages/OfferDetails";
 import App from "./App";
 import ConnexionPage from "./pages/ConnexionPage";
@@ -10,13 +11,15 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/offers/:id" element={<OfferDetails />} />
-        <Route path="/connexion" element={<ConnexionPage />} />
-        <Route path="/createaccount/:type" element={<CreateAccount />} />
-      </Routes>
-    </Router>
+    <AuthContextProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/offers/:id" element={<OfferDetails />} />
+          <Route path="/connexion" element={<ConnexionPage />} />
+          <Route path="/createaccount/:type" element={<CreateAccount />} />
+        </Routes>
+      </Router>
+    </AuthContextProvider>
   </React.StrictMode>
 );
