e0445540a6843b30658a7488fa676e041ce8d909
wild-valentin <marlois.valentin@gmail.com>
2023-01-18 11:39:06 +0100

    addition of the menu when you are connected and links

--- frontend/src/pages/Profil.jsx
+++ frontend/src/pages/Profil.jsx
@@ -1,12 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { AuthContext } from "./AuthContext";
 import CandidateArea from "./CandidateArea";
 import ConsultantArea from "./ConsultantArea";
 
 function Profil() {
   const { type } = useParams();
+  const { auth } = useContext(AuthContext);
+  const navigate = useNavigate();
   /**
    * type : consultant || candidate
    */
+  if (!auth.isAuthenticated) {
+    navigate("/");
+  }
   return (
     <>
       {type === "consultant" && <ConsultantArea />}
