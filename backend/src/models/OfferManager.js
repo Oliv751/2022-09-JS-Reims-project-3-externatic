2012cc8de9e8b8cc579e69df372a97d78f684bbf
iturnmyscreenon <benoit.galey@gmail.com>
2023-01-20 10:55:31 +0100

    modify offermanager

--- backend/src/models/OfferManager.js
+++ backend/src/models/OfferManager.js
@@ -4,6 +4,28 @@ class OfferManager extends AbstractManager {
   constructor() {
     super({ table: "offer" });
   }
+
+  insert(data) {
+    const {
+      companyDescription,
+      offerName,
+      offerDescription,
+      contract,
+      location,
+      consultantId,
+    } = data;
+    return this.connection.query(
+      `INSERT ${this.table} SET company_description = ?, offer_name = ?, offer_description = ?, contract = ?, location = ?, consultant_id = ?`,
+      [
+        companyDescription,
+        offerName,
+        offerDescription,
+        contract,
+        location,
+        consultantId,
+      ]
+    );
+  }
 }
 
 module.exports = OfferManager;
