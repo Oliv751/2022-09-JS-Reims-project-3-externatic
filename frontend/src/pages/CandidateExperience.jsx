83e56e391fc7ccb8804d6847958f3ffa810b88d2
Oliv751 <olivierlopez751@gmail.com>
2023-01-24 17:07:13 +0100

    hide/show experience form and some styles

--- frontend/src/pages/CandidateExperience.jsx
+++ frontend/src/pages/CandidateExperience.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
+import { HiOutlinePlusCircle, HiOutlineMinusCircle } from "react-icons/hi";
 import { AuthContext } from "./AuthContext";
 import Header from "../components/Header";
 import "../styles/candidateExperience.scss";
@@ -82,83 +83,103 @@ function Experience() {
       });
   };
 
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Header />
       <section className="experience_capture">
-        <h1>Ajouter une expérience</h1>
-        <form onSubmit={handleSubmit}>
-          <label
-            className="form-candidate-experience-input-category"
-            htmlFor="category_id"
-          >
-            Categorie
-          </label>
-          <select name="category_id" onChange={handleChange} id="select">
-            {categories.map((category) => (
-              <option key={category.id} value={category.id}>
-                {category.name}
-              </option>
-            ))}
-          </select>
+        <h2 className="exp-title">
+          Ajouter une expérience
+          {showForm ? (
+            <HiOutlineMinusCircle
+              className="minus-icon"
+              onClick={() => setShowForm(false)}
+            />
+          ) : (
+            <HiOutlinePlusCircle
+              className="plus-icon"
+              onClick={() => setShowForm(true)}
+            />
+          )}
+        </h2>
+        {showForm && (
+          <form onSubmit={handleSubmit}>
+            <label className="job-category" htmlFor="category_id">
+              Categorie
+              <select name="category_id" onChange={handleChange} id="select">
+                {categories.map((category) => (
+                  <option key={category.id} value={category.id}>
+                    {category.name}
+                  </option>
+                ))}
+              </select>
+            </label>
 
-          <label className="job-name">
-            Nom du poste :
-            <input
-              type="text"
-              value={formData.job_name}
-              onChange={handleChange}
-              name="job_name"
-            />
-          </label>
-          <label className="company-name">
-            Nom de l'entreprise :
-            <input
-              type="text"
-              value={formData.company_name}
-              onChange={handleChange}
-              name="company_name"
-            />
-          </label>
-          <label className="experience-description">
-            Description de l'expérience :
-            <textarea
-              value={formData.experience_description}
-              onChange={handleChange}
-              name="experience_description"
-            />
-          </label>
-          <label className="start-date">
-            Date de début :
-            <input
-              type="date"
-              value={formData.startDate}
-              onChange={handleChange}
-              name="startDate"
-            />
-          </label>
-          <label className="end-date">
-            Date de fin :
-            <input
-              type="date"
-              value={formData.endDate}
-              onChange={handleChange}
-              name="endDate"
-            />
-          </label>
-          <button className="submit-button" type="submit">
-            Ajouter
-          </button>
-        </form>
-        <h2>Mes expériences professionnelles</h2>
+            <label className="job-name">
+              Nom du poste :
+              <input
+                type="text"
+                value={formData.job_name}
+                onChange={handleChange}
+                name="job_name"
+              />
+            </label>
+            <label className="company-name">
+              Nom de l'entreprise :
+              <input
+                type="text"
+                value={formData.company_name}
+                onChange={handleChange}
+                name="company_name"
+              />
+            </label>
+            <label className="experience-description">
+              Description de l'expérience :
+              <textarea
+                value={formData.experience_description}
+                onChange={handleChange}
+                name="experience_description"
+              />
+            </label>
+            <label className="start-date">
+              Date de début :
+              <input
+                type="date"
+                value={formData.startDate}
+                onChange={handleChange}
+                name="startDate"
+              />
+            </label>
+            <label className="end-date">
+              Date de fin :
+              <input
+                type="date"
+                value={formData.endDate}
+                onChange={handleChange}
+                name="endDate"
+              />
+            </label>
+            <button
+              className="submit-button"
+              type="submit"
+              onClick={() => setShowForm(false)}
+            >
+              Ajouter
+            </button>
+          </form>
+        )}
+      </section>
+      <section className="experience-list">
+        <h2 className="exp-title">Mes expériences professionnelles</h2>
         {experiences.map((experience) => (
-          <section className="experience_list" key={experience.id}>
+          <div className="one-experience" key={experience.id}>
             <h3>{experience.job_name}</h3>
             <p>{experience.company_name}</p>
             <p>{experience.experience_description}</p>
             <p>Date de début : {experience.startDate}</p>
             <p>Date de fin : {experience.endDate}</p>
-          </section>
+          </div>
         ))}
       </section>
     </>
