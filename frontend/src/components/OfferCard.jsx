ebe12e4779162c20c47195cb027208ccb39e1fd4
FMichel5173 <fmichel81@sfr.fr>
2023-01-27 09:30:17 +0100

    Modification of the OfferCard and OfferDetail style and reorganize code

--- frontend/src/components/OfferCard.jsx
+++ frontend/src/components/OfferCard.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { MdLocationOn } from "react-icons/md";
 import { FaFileSignature } from "react-icons/fa";
 import SampleCard from "./sampleCard";
@@ -9,28 +8,22 @@ export default function OfferCard({ offer }) {
   const date = new Date(offer.publication_date);
 
   return (
-    <li>
-      <Link to={`/offers/${offer.id}`} className="cardsOffer">
-        <section>
-          <div className="description">
-            <h2>{offer.offer_name}</h2>
+    <section className="cardsOffer">
+      <header>
+        <h2>{offer.offer_name}</h2>
 
-            <p className="published">
-              <span>publié le </span>
-              {date.toLocaleDateString("fr-FR")}
-            </p>
-          </div>
-          <div>
-            <MdLocationOn className="icon_description" />
-            <p>{offer.location}</p>
-          </div>
-          <div>
-            <FaFileSignature className="icon_description" />
-            <p>{offer.contract}</p>
-          </div>
-        </section>
-      </Link>
-    </li>
+        <time className="published">{date.toLocaleDateString("fr-FR")}</time>
+      </header>
+      <p className="description">
+        {offer.offer_description.split(" ").slice(0, 20).join(" ")}...
+      </p>
+      <footer>
+        <MdLocationOn className="icon_description" />
+        <p>{offer.location}</p>
+        <FaFileSignature className="icon_description" />
+        <p>{offer.contract}</p>
+      </footer>
+    </section>
   );
 }
 
